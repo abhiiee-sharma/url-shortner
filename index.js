c9fea29616c15d33e6399e3ddf0e1d2f5f33b39f
Abhishek Sharma <dev.abhiiee@gmail.com>
2025-04-10 14:54:56 +0530

    Adopted changes to deploy this on render

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+require('dotenv').config(); // Add this line at the top
+
 const express = require("express");
 
 const connectMongoDb = require("./connection");
 
 const{} = require('./models/url');
+const helmet = require('helmet');
 
 const urlRoute = require('./routes/url');
 const staticRoute = require('./routes/staticRouter')
@@ -10,20 +13,26 @@ const path = require('path');
 
 
 const app = express();
-const PORT = 8001;
+const PORT = process.env.PORT || 8001; 
+const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/url-shortner"; // Use environment variable for MongoDB URI
 
 
 
 //Mongoose connection
-connectMongoDb("mongodb://127.0.0.1:27017/url-shortner").then(() => {
-  console.log("MongoDB connected!");
-});
+connectMongoDb(MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected!");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+    process.exit(1); // Exit the process if the database connection fails
+  });
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 //Middleware to fetch data
-// app.use(express.urlencoded({extended: false}));
+app.use(helmet());
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
@@ -33,6 +42,8 @@ app.use("/", staticRoute);
 
 
 //Server connected on port
-app.listen(8001, () => {
-    console.log(`Server started on port : ${PORT}`);  
-})
+app.listen(PORT, () => {
+  console.log(`Server started on port : ${PORT}`);
+}).on('error', (err) => {
+  console.error("Failed to start server", err);
+});
